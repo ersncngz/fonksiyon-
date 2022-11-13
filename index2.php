706189f6cee31c9733e4c2ae2d1073186c5e799c
ersan cengiz <ersan.cengiz16@gmail.com>
2022-11-13 21:16:44 +0300

    ilk commit

---
+++ index2.php
@@ -0,0 +1,17 @@
+<?php
+function sayiciz($sayi1){
+    for ($i = 0; $i <= $sayi1; $i++){
+      $j = 0;
+        while( $j <= $i )
+        {
+        
+          echo" *";
+          $j++;
+        }
+
+         echo "<br>";
+                        }
+                      }
+    sayiciz(15);
+
+?>
